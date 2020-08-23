5441f95c7178cc717d4ee4477f67dcb901134c14
LALA <sh442555789@gmail.com>
2020-08-23 00:28:08 -0700

    saturday

--- robin.py
+++ robin.py
@@ -1,5 +1,6 @@
 from heapq import heappush,heappop
 from collections import deque
+import random
 # matrix rotate, gravity
 def gravity(matrix):
     
@@ -173,6 +174,167 @@ def coolFeature(a,b,query):
     print('coolfeature',result)
     return result
 
+def justifyNewpaper(lines,align,width):
+    result = ["*"*(width+2)]
+    for i in range(len(lines)):
+        curr = []
+        length = 0
+        
+        for word in lines[i]:
+            
+            if len(word)+ length + len(curr)-1 >width:
+                if align[i] == "LEFT":
+                    result.append("*"+' '.join(curr).ljust(width)+"*")
+                else:
+                    result.append("*"+' '.join(curr).rjust(width)+"*")
+                curr = []
+                length = 0
+            curr.append(word)
+            length += len(word)
+        
+        #print(curr,align[i])
+        if align[i] == "LEFT":
+            #print('left,curr',curr)
+            result.append("*" +' '.join(curr).ljust(width)+"*")
+        else:
+            result.append("*"+' '.join(curr).rjust(width)+"*")
+    result.append("*"*(width+2))   
+    for r in result:
+        print(r)
+
+def kOccurence(sequence,words):
+    # sliding window
+    result = []
+    for word in words:
+        
+        start,end = 0,0
+        k = 0
+        gmax = 0
+        count = 0
+        while end < len(sequence):
+            if sequence[end] ==word[k]:
+                #print(sequence[end])
+                end += 1
+                k += 1
+                if k == len(word):
+                    k = 0
+                    count += 1
+                    gmax = max(gmax,count)
+            else:
+                count = 0
+                k = 0 if word[0]!= sequence[end] else 1
+                end += 1
+            
+        result.append(gmax)
+            
+    print(result)
+
+def pairSum2(array):
+    power = []
+    for i in range(32):
+        power.append(2**i)
+    result = 0
+    print(array)
+    for p in power:
+        check = set([])
+        for num in array:
+            check.add(p-num)
+            if num in check:
+                result += 1
+            
+                print(p-num,num,p)
+            
+    print(result)
+    
+def gotoMall(n,m,x1,y1,x2,y2):
+    matrix = list(map(lambda x: [0]*m ,range(n)))
+    print('start',y1,x1,'end',y2,x2)
+    for ma in matrix:
+        print(ma)
+    i,j = y1,x1
+    destX,destY = y2,x2
+    def isValid(x,y,direc):
+        rev ={
+            -1 : 1,
+            1 : -1
+        }
+        
+        if (x + direc[0] <0 or x+direc[0] >=n) and ( y+direc[1] <0 or y+direc[1]>=m ):
+            return [rev[direc[0]],rev[direc[1]]]
+        elif x+direc[0] <0 or x+direc[0] >=n :
+            return [rev[direc[0]],direc[1]]
+        elif y+direc[1] <0 or y+direc[1]>=m :
+            return [direc[0], rev[direc[1]]]
+        
+        return direc
+    direc = [-1,1]
+    went = {}
+    count = 0
+    while not (i== destX and j == destY):
+        
+        # -1, 1
+        
+        if matrix[i][j] == 3:
+            break
+        matrix[i][j] +=1
+        print(i,j,'step',count)
+        for ma in matrix:
+            print(ma)
+        direc = isValid(i,j,direc) 
+        i += direc[0]
+        j += direc[1]
+        #print('next',i,j,direc)
+        count +=1 
+    
+    if i==destX and j == destY:
+        print(True,i,j,"step",count)
+    else:
+        print(False,i,j,"step",count)
+    
+from collections import defaultdict
+def revPair(array):
+    check = defaultdict(list)
+    for a in array:
+        tmp =str(a) 
+    """
+    hashmap {
+        a-rev(a)
+        0: [a...]
+        1: [a...]
+    }
+    every key find combination 2
+    """
+
+def countSplitArray(array):
+    n = len(array)
+    prefix = [0]*n
+    sufix = [0]*n
+    
+    count = 0
+    for i in range(n):
+        count += array[i]
+        prefix[i] = count
+    
+    count = 0
+    for i in range(n-1,-1,-1):
+        count+= array[i]
+        sufix[i] = count
+    print(array)
+    print(prefix)
+    print(sufix)
+    s,e = 1,1
+    currSubArrSum, count = 0,0
+    while s < n-1 and e <n-1:
+        while e < n-1 and currSubArrSum < prefix[s-1]:
+            currSubArrSum+= array[e]
+            e+= 1
+        if currSubArrSum <= sufix[e]:
+            count += 1
+        
+        currSubArrSum -= array[s]
+        s += 1
+    print(count)
+#def sawTooth(array):
 
 
     
@@ -202,8 +364,21 @@ if __name__ == "__main__":
     coola=[1,2,2]
     coolb=[2,3]
     coolq=[[1,4],[0,0,3],[1,5]]
-    coolFeature(coola,coolb,coolq)
+    #coolFeature(coola,coolb,coolq)
     #textEditor(operations)
     #restoreOrder(topoPair)
-
+    lines = [["hello", "world","la","jfiwng","jfienguj"],["how","areYou","doing"],["Please look","and align", "to right"]]
+    aligns = ["LEFT","RIGHT","RIGHT"]
+    width = 16
+    #justifyNewpaper(lines,aligns,width)
+    sequence = "abcdaaaa"
+    words =["ab", "abcd","bca","a"]
+    #kOccurence(sequence,words)
+    pairPower=[-2,-1,0,1,2]
+    mallN = random.randint(5,10)
+    mallM = random.randint(5,10)
+    #gotoMall(mallN,mallM,random.randint(0,mallM-1),random.randint(0,mallN-1),random.randint(0,mallM-1),random.randint(0,mallN-1))
+    split3Array = [1,1,1]
+    countSplitArray(split3Array)
+    #pairSum2(pairPower)
     #deleteMinimumPeak(miniPeak)
